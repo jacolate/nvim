7a4b02d5ef55850a0f6a57dae6bf163f36ed586a
Oreok <jakobkoller@t-online.de>
2025-05-20 23:50:53 +0200

    add markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = function()
+    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+    vim.fn["mkdp#util#install"]()
+  end,
+  config = function()
+    vim.cmd([[do FileType]])
+  end,
+}
